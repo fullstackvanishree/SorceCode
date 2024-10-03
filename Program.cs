77f991b5013cf76b46356b182bcf59970e577231
vani shree <hvanishree.20@gmail.com>
2024-10-03 22:25:30 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+//#include<stdio.h>
+
+namespace ConsoleAppDotnet5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
